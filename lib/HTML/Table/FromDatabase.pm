e1eef0e2677d2ac58a305c453cff3508132ecb67
David Precious <davidp@preshweb.co.uk>
2008-08-21 21:59:35 +0000

    Document the new callbacks feature.
    git-svn-id: file:///shared/svn/scripts/perl/modules/HTML-Table-FromDatabase@422 c1e6fe2f-0919-0410-96f0-e3221d39872c

--- lib/HTML/Table/FromDatabase.pm
+++ lib/HTML/Table/FromDatabase.pm
@@ -48,7 +48,9 @@ so you can use all the usual HTML::Table features.
 
 Constructor method - consult L<HTML::Table>'s documentation, the only
 difference here is the addition of the required I<-sth> parameter which
-should be a DBI statement handle.
+should be a DBI statement handle, and the optional I<-callbacks> parameter
+which specifies callbacks/transformations which should be applied as the
+table is built up (see the callbacks section below).
 
 =cut
 
@@ -165,10 +167,51 @@ __END__;
 
 =back
 
+=head1 CALLBACKS
+
+You can pass an arrayref of hashrefs describing callbacks to be performed as
+the table is built up, which can modify the data before the table is produced.
+
+This can be very useful; one example use-case would be turning the values in
+a column which contains URLs into clickable links:
+
+ my $table = HTML::Table::FromDatabase->new(
+    -sth => $sth,
+    -callbacks => [
+        {
+            column => 'url',
+            transform => sub { $_ = shift; qq[<a href="$_">$_</a>]; },
+        },
+    ],
+ );
+
+You can match against the column name using a key named column in the hashref
+(as illustrated above) or against the actual value using a key named value.
+
+You can pass a straight scalar to compare against, a regex (using qr//), or
+a coderef which will be executed to determine if it matches.
+
+Another example - displaying all numbers to two decimal points:
+
+ my $table = HTML::Table::FromDatabase->new(
+    -sth => $sth,
+    -callbacks => [
+        {
+            value => qr/\d+/,
+            transform => sub { return sprintf '%.2f', shift },
+        },
+    ],
+ );
+
+It is hoped that this facility will allow the easyness of quickly creating
+a table to still be retained, even when you need to do things with the data
+rather than just displaying it exactly as it comes out of the database.
 =head1 AUTHOR
 
 David Precious, E<lt>davidp@preshweb.co.ukE<gt>
 
+Feel free to contact me if you have any comments, suggestions or bugs to
+report.
 
 =head1 COPYRIGHT AND LICENSE
 
@@ -177,4 +220,3 @@ Copyright (C) 2008 by David Precious
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.7 or,
 at your option, any later version of Perl 5 you may have available.
-
